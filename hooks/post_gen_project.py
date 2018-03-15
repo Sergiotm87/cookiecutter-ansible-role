a9c75d4f654c9ec195745a43fd96fdf64b1bf617
sergiotm87 <sergiotm87@gmail.com>
2018-03-15 17:40:39 +0100

    hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from git import Repo
 
-role_name = '{{ cookiecutter.role_name }}'
+role_name = {{cookiecutter.role_name}}
 
-Repo.clone_from("https://github.com/Sergiotm87/kitchen-ansible-WordpressNginx", "role_name/")
+Repo.clone_from("https://github.com/Sergiotm87/kitchen-ansible-WordpressNginx", role_name)
