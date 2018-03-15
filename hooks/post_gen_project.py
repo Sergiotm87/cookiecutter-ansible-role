790a00f1a1d3949d7337850fd0b9e1b92dfeb5ec
sergiotm87 <sergiotm87@gmail.com>
2018-03-15 18:09:39 +0100

    hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
 from git import Repo
+import os
 
-Repo.clone_from('https://github.com/Sergiotm87/kitchen-ansible-WordpressNginx', '{{cookiecutter.role_name}}', branch='master')
+print os.getcwd()
+
+Repo.clone_from('https://github.com/Sergiotm87/kitchen-ansible-WordpressNginx', '../{{cookiecutter.role_name}}', branch='master')
