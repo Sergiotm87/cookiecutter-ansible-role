fc2d220e489c61a28fb8fcb4a331243a58c85f5a
sergiotm87 <sergiotm87@gmail.com>
2018-03-15 18:53:42 +0100

    hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 from git import Repo
 import os
+import shutil
 
 Repo.clone_from('https://github.com/Sergiotm87/kitchen-ansible-WordpressNginx', '{{cookiecutter.role_name}}', branch='master')
 
-print os.getcwd()
+path = os.getcwd()+"/{{cookiecutter.role_name}}/"
+moveto = os.getcwd()+"/"
+files = os.listdir(path)
+files.sort()
+for f in files:
+    src = path+f
+    dst = moveto+f
+    shutil.move(src,dst)
+os.rmdir({{cookiecutter.role_name}})
