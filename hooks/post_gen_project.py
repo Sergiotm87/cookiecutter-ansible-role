f35e74beefc6edbdb6665e1b936f46e96221f210
sergiotm87 <sergiotm87@gmail.com>
2018-03-17 21:00:31 +0100

    python git

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from git import Repo,remote
+import git
 import os
 import shutil
 from github import Github
@@ -7,7 +8,7 @@ from github import Github
 #clone repository
 Repo.clone_from('{{cookiecutter.ansible_url}}', '{{cookiecutter.role_name}}', branch='master')
 
-#copy all files from repository to working directory (cannot clone into not empy directory)
+#copy all files from repository to working directory (cannot clone into not empty directory)
 path = os.getcwd()+"/{{cookiecutter.role_name}}/"
 moveto = os.getcwd()+"/"
 files = os.listdir(path)
@@ -17,14 +18,24 @@ for f in files:
     dst = moveto+f
     shutil.move(src,dst)
 os.rmdir('{{cookiecutter.role_name}}')
+os.rmdir('.git')
 
 #create a repo using name/pass or token
 #g = Github(os.environ['gituser'], os.environ['gitpass'])
 g = Github(os.environ['gittoken'])
 u = g.get_user()
-newrepo = u.create_repo('{{cookiecutter.repo_name}}')
+u.create_repo('{{cookiecutter.repo_name}}')
 
-#push to new repo
-repo = Repo(os.getcwd(), search_parent_directories=True)
-remote = repo.create_remote('testapi', url='https://'+os.environ['gituser']+':'+os.environ['gitpass']+'@github.com/'+'{{cookiecutter.github_user}}'+'/'+'{{cookiecutter.repo_name}}')
-remote.push(refspec='{}:{}'.format('master', 'master'))
+# create working directory repo
+git.Repo.init(os.getcwd())
+
+# get working diretory repo
+repo = git.Repo(os.getcwd())
+# add files, remote, commit and push
+repo.git.add('.')
+repo.index.commit("wei commit")
+remote = repo.create_remote('origin', url='https://'+os.environ['gituser']+':'+os.environ['gitpass']+'@github.com/'+'{{cookiecutter.github_user}}'+'/'+'{{cookiecutter.repo_name}}'+'.git')
+remote.push(refspec='{}:{}'.format('master', 'origin'))
+
+#T O D O
+# no guardar en el fichero las variables de entorno
